85cd9e291ff9249770ca8c661638fb4779949725
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2019-03-27 16:26:34 -0400

    initial heroku push

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://merlz:winner66@ds163294.mlab.com:63294/heroku_gj1hvh6d"
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+const express =  require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+// ------ MIDDLEWARE --------- //
+app.use(
+  bodyParser.urlencoded({
+    extended:false
+  })
+);
+app.use(bodyParser.json());
+
+// ------ DB CONNECTION -------- //
+const db = require("./config/keys").mongoURI;
+
+mongoose.connect(
+  db, {useNewUrlParser:true}
+).then(()=> console.log("MONGO DB CONNECTED"))
+.catch(err => console.log(err));
+
+
+// ----- Heroku's port and local ----- //
+const port = process.env.port || 3000;
+
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
