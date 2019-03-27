3a860d966fdbd2f64a332e52e648603657522160
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2019-03-27 17:04:40 -0400

    fixing merge

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://merlz:winner66@ds163294.mlab.com:63294/heroku_gj1hvh6d",
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express =  require('express');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -20,6 +22,14 @@ mongoose.connect(
   db, {useNewUrlParser:true}
 ).then(()=> console.log("MONGO DB CONNECTED"))
 .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+
 
 
 // ----- Heroku's port and local ----- //
